0bc33aafb4937fbb758a599b5a0b115091a23112
Girish <girishm60@gmail.com>
2021-10-18 14:45:20 -0400

    updated the script

--- delete_pieces.ps1
+++ delete_pieces.ps1
@@ -1,3 +1,5 @@
+#Run the script by using .\delete_pieces.ps1 in the root directory of the project.
+
 $numbers=1001, 1002, 1003, 1004, 1005
 foreach ($num in $numbers) {
   Remove-Item .\$num\piece_*

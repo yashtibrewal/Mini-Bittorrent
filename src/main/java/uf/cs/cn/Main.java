03a396b0db073fd03b1aa03b7a82a641e7085435
Girish <girishm60@gmail.com>
2021-10-18 17:06:27 -0400

    added peer_id through command line arguments

--- src/main/java/uf/cs/cn/Main.java
+++ src/main/java/uf/cs/cn/Main.java
@@ -6,9 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
         // pass the peer_id to constructor from CLA
+        int peer_id;
+        try {
+            peer_id = extractPeerId(args);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
         Peer server_instance = new Peer(true);
         server_instance.start();
         Peer client_instance = new Peer(false);
         client_instance.start();
+    }
+
+    public static int extractPeerId(String[] args) throws Exception {
+        if (args.length == 0 || args.length > 1) {
+            throw new Exception("Invalid command line arguments passed");
+        }
+        return Integer.parseInt(args[0]);
     }
 }

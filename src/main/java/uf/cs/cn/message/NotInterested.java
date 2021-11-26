78e0313961bea2e949ba016525f79aa8e102d1d0
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2021-11-26 18:33:28 -0500

    removed wrappers, have message impleemted

--- src/main/java/uf/cs/cn/message/NotInterested.java
+++ src/main/java/uf/cs/cn/message/NotInterested.java
@@ -1,19 +1,7 @@
 package uf.cs.cn.message;
 
-import java.io.IOException;
-
 public class NotInterested extends ActualMessage{
-
     public NotInterested() throws Exception {
         super(1, MessageType.NOT_INTERESTED);
-    }
-
-    public byte[] getNotInterestedMessageBytes(){
-        try {
-            return getEncodedMessage();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
